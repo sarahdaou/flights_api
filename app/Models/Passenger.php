35413e85588b11a48347b4481a239c46424e252e
Sarah Daou <adaou.sarah@gmail.com>
2024-06-18 10:35:47 +0300

    updated  property in model

--- app/Models/Passenger.php
+++ app/Models/Passenger.php
@@ -9,4 +9,12 @@ class Passenger extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'date_of_birth',
+        'passport_expiry_date'
+    ];
 };
