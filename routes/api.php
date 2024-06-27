2ed6e0c459478d88f0e44e4251221751fe064f2c
Sarah Daou <adaou.sarah@gmail.com>
2024-06-27 15:40:10 +0300

    authentication and signup,login,logout added

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FlightController;
 use App\Http\Controllers\PassengerController;
+use App\Http\Controllers\AuthenticationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,22 +22,31 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/passengers', [PassengerController::class, 'index']);
+// Route::get('/flights', [FlightController::class, 'index']);
+// Route::get('/flights/{flight}', [FlightController::class, 'show']);
+// Route::post('/flights', [FlightController::class, 'store']);
+// Route::put('/flights/{flight}', [FlightController::class, 'update']);
+// Route::delete('/flights/{flight}', [FlightController::class, 'destroy']);
 
-Route::get('/flights', [FlightController::class, 'index']);
+// Route::get('/passengers', [PassengerController::class, 'index']);
+// Route::get('/passengers/{passenger}', [PassengerController::class, 'show']);
+// Route::post('/passengers', [PassengerController::class, 'store']);
+// Route::put('/passengers/{passenger}', [PassengerController::class, 'update']);
+// Route::delete('/passengers/{passenger}', [PassengerController::class, 'destroy']);
 
-Route::get('/flights/{flight}', [FlightController::class, 'show']);
+Route::post('/register', [AuthenticationController::class, 'register']);
+Route::post('/login', [AuthenticationController::class, 'login']);
+Route::middleware('auth:sanctum')->post('/logout', [AuthenticationController::class, 'logout']);
 
-Route::post('/flights', [FlightController::class, 'store']);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
-Route::put('/flights/{flight}', [FlightController::class, 'update']);
+Route::apiResource('passengers', PassengerController::class);
+Route::apiResource('flights', PassengerController::class);
 
-Route::delete('/flights/{flight}', [FlightController::class, 'destroy']);
-
-Route::get('/passengers/{passenger}', [PassengerController::class, 'show']);
-
-Route::post('/passengers', [PassengerController::class, 'store']);
-
-Route::put('/passengers/{passenger}', [PassengerController::class, 'update']);
-
-Route::delete('/passengers/{passenger}', [PassengerController::class, 'destroy']);
+// GET           /users                      index   users.index
+// POST          /users                      store   users.store
+// GET           /users/{user}               show    users.show
+// PUT|PATCH     /users/{user}               update  users.update
+// DELETE        /users/{user}               destroy users.destroy
